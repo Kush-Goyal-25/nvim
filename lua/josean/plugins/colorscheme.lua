2a7c723bab385815ba8880f5fb17102037c93a62
Kush Goyal <kush.goyal.personal@gmail.com>
2025-02-04 21:58:52 +0530

    Add files via upload

--- lua/josean/plugins/colorscheme.lua
+++ lua/josean/plugins/colorscheme.lua
@@ -1,60 +1,102 @@
+-- -- Define a function to set the color scheme and make the background transparent
+-- function ColorMyPencils(color)
+--   color = color or "rose-pine" -- Default to "rose-pine" if no color is provided
+--   vim.cmd.colorscheme(color)
+--
+--   -- Set background transparency for Normal and NormalFloat highlight groups
+--   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- end
+--
+-- -- Return the plugins configuration
+-- return {
+--   -- Tokyonight theme configuration
+--   {
+--       "folke/tokyonight.nvim",
+--       config = function()
+--           require("tokyonight").setup({
+--               -- Customize the Tokyonight theme
+--               style = "storm", -- Options: "storm", "moon", "night", "day"
+--               transparent = true, -- Make the background transparent
+--               terminal_colors = true, -- Use theme colors in terminal
+--               styles = {
+--                   comments = { italic = false }, -- Disable italics for comments
+--                   keywords = { italic = false }, -- Disable italics for keywords
+--                   sidebars = "dark", -- Use "dark" style for sidebars
+--                   floats = "dark", -- Use "dark" style for floating windows
+--               },
+--           })
+--
+--           -- Apply the theme after setup
+--           vim.cmd("colorscheme tokyonight")
+--       end
+--   },
+--
+--   -- Rose-pine theme configuration
+--   {
+--       "rose-pine/neovim",
+--       name = "rose-pine", -- Specify the plugin name
+--       config = function()
+--           require('rose-pine').setup({
+--               disable_background = true, -- Disable the background color
+--           })
+--
+--           -- Apply the rose-pine theme and call ColorMyPencils
+--           vim.cmd("colorscheme rose-pine")
+--           ColorMyPencils()
+--       end
+--   },
+-- }
+
 -- Define a function to set the color scheme and make the background transparent
 function ColorMyPencils(color)
-  color = color or "rose-pine" -- Default to "rose-pine" if no color is provided
-  vim.cmd.colorscheme(color)
+     color = color or "rose-pine" -- Default to "rose-pine" if no color is provided
 
-  -- Set background transparency for Normal and NormalFloat highlight groups
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+     -- Set background transparency for Normal and NormalFloat highlight groups
+     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 -- Return the plugins configuration
 return {
-  -- Tokyonight theme configuration
-  {
-      "folke/tokyonight.nvim",
-      config = function()
-          require("tokyonight").setup({
-              -- Customize the Tokyonight theme
-              style = "storm", -- Options: "storm", "moon", "night", "day"
-              transparent = true, -- Make the background transparent
-              terminal_colors = true, -- Use theme colors in terminal
-              styles = {
-                  comments = { italic = false }, -- Disable italics for comments
-                  keywords = { italic = false }, -- Disable italics for keywords
-                  sidebars = "dark", -- Use "dark" style for sidebars
-                  floats = "dark", -- Use "dark" style for floating windows
-              },
-          })
+     -- Tokyonight theme configuration
+     {
+          "folke/tokyonight.nvim",
+          config = function()
+               require("tokyonight").setup({
+                    -- Customize the Tokyonight theme
+                    style = "storm", -- Options: "storm", "moon", "night", "day"
+                    transparent = true, -- Make the background transparent
+                    terminal_colors = true, -- Use theme colors in terminal
+                    styles = {
+                         comments = { italic = false }, -- Disable italics for comments
+                         keywords = { italic = false }, -- Disable italics for keywords
+                         sidebars = "dark", -- Use "dark" style for sidebars
+                         floats = "dark", -- Use "dark" style for floating windows
+                    },
+               })
 
-          -- Apply the theme after setup
-          vim.cmd("colorscheme tokyonight")
-      end
-  },
+               -- Apply the theme and set transparency
+               vim.cmd("colorscheme tokyonight")
+               ColorMyPencils("tokyonight")
+          end,
+     },
 
-  -- Rose-pine theme configuration
-  {
-      "rose-pine/neovim",
-      name = "rose-pine", -- Specify the plugin name
-      config = function()
-          require('rose-pine').setup({
-              disable_background = true, -- Disable the background color
-          })
+     -- Rose-pine theme configuration
+     {
+          "rose-pine/neovim",
+          name = "rose-pine", -- Specify the plugin name
+          config = function()
+               require("rose-pine").setup({
+                    disable_background = true, -- Disable the background color
+               })
 
-          -- Apply the rose-pine theme and call ColorMyPencils
-          vim.cmd("colorscheme rose-pine")
-          ColorMyPencils()
-      end
-  },
+               -- Apply the theme and set transparency
+               vim.cmd("colorscheme rose-pine")
+               ColorMyPencils("rose-pine")
+          end,
+     },
 }
-
-
-
-
-
-
-
-
 
 -- return {
 --   {
