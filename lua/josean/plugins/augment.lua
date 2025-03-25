b21880389a053de58132c0c33d4e5bfb1064c131
Kush Goyal <kush.goyal.personal@gmail.com>
2025-03-25 18:43:25 +0530

    Add files via upload

---
+++ lua/josean/plugins/augment.lua
@@ -0,0 +1,40 @@
+return {
+     "augmentcode/augment.vim",
+     event = "BufEnter", -- Optional: Lazy-load on buffer entry
+     config = function()
+          -- Keymaps for Augment functionality
+
+          -- Use Alt-Y to accept a suggestion
+          vim.keymap.set("i", "<C-y>", function()
+               vim.schedule(function()
+                    vim.fn["augment#Accept"]()
+               end)
+               return ""
+          end, { expr = true, silent = true, desc = "Accept Augment completion" })
+
+          -- Use Enter to accept a suggestion, falling back to a newline if no suggestion is available
+          vim.keymap.set("i", "<C-n>", function()
+               vim.schedule(function()
+                    vim.fn["augment#Accept"]("\n")
+               end)
+               return ""
+          end, { expr = true, silent = true, desc = "Accept Augment completion or insert newline" })
+
+          -- Normal mode: Open Augment chat
+          vim.keymap.set("n", "<leader>ac", ":Augment chat<CR>", { silent = true, desc = "Open Augment chat" })
+
+          -- Visual mode: Open Augment chat with selected text
+          vim.keymap.set(
+               "v",
+               "<leader>ac",
+               ":Augment chat<CR>",
+               { silent = true, desc = "Open Augment chat with selection" }
+          )
+
+          -- Normal mode: Start a new Augment chat
+          vim.keymap.set("n", "<leader>an", ":Augment chat-new<CR>", { silent = true, desc = "Start new Augment chat" })
+
+          -- Normal mode: Toggle Augment chat
+          vim.keymap.set("n", "<leader>at", ":Augment chat-toggle<CR>", { silent = true, desc = "Toggle Augment chat" })
+     end,
+}
