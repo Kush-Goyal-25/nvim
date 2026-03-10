73bf75fa2ddb5e618ed7228dadd2e5e6ad10e77d
Kush Goyal <kush.goyal.personal@gmail.com>
2026-03-10 23:06:02 +0530

    Add files via upload

--- lua/josean/plugins/telescope.lua
+++ lua/josean/plugins/telescope.lua
@@ -10,17 +10,26 @@ return {
      config = function()
           local telescope = require("telescope")
           local actions = require("telescope.actions")
+          local builtin = require("telescope.builtin")
 
           telescope.setup({
                defaults = {
                     path_display = { "smart" },
-                    -- Avoid ft_to_lang nil error with nvim-treesitter main (Telescope 0.1.x incompatibility)
                     preview = { treesitter = false },
                     mappings = {
                          i = {
-                              ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-                              ["<C-j>"] = actions.move_selection_next, -- move to next result
-                              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                              ["<C-k>"] = actions.move_selection_previous,
+                              ["<C-j>"] = actions.move_selection_next,
+                              ["<C-q>"] = function(prompt_bufnr)
+                                   actions.smart_send_to_qflist(prompt_bufnr)
+                                   vim.cmd("Trouble quickfix toggle")
+                              end,
+                         },
+                         n = {
+                              ["<C-q>"] = function(prompt_bufnr)
+                                   actions.smart_send_to_qflist(prompt_bufnr)
+                                   vim.cmd("Trouble quickfix toggle")
+                              end,
                          },
                     },
                },
@@ -28,14 +37,14 @@ return {
 
           telescope.load_extension("fzf")
 
-          -- set keymaps
-          local keymap = vim.keymap -- for conciseness
+          local keymap = vim.keymap
 
           keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
           keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
           keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
           keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+          keymap.set("n", "<leader>vh", builtin.help_tags, { desc = "Search help tags" })
+          keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Search git files" })
           keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
      end,
 }
-
