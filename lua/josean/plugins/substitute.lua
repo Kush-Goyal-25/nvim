d86cf0bcf4862c8db1d5f1c3a849fc6ff4640fdc
Kush Goyal <kush.goyal.personal@gmail.com>
2026-03-16 16:01:27 +0530

    Add files via upload

--- lua/josean/plugins/substitute.lua
+++ lua/josean/plugins/substitute.lua
@@ -9,10 +9,9 @@ return {
           -- set keymaps
           local keymap = vim.keymap -- for conciseness
 
-          keymap.set("n", "ns", substitute.operator, { desc = "Substitute with motion" })
-          keymap.set("n", "nss", substitute.line, { desc = "Substitute line" })
-          keymap.set("n", "nS", substitute.eol, { desc = "Substitute to end of line" })
-          keymap.set("x", "ns", substitute.visual, { desc = "Substitute in visual mode" })
+          keymap.set("n", "<leader>ns", substitute.operator, { desc = "Substitute with motion" })
+          keymap.set("n", "<leader>nss", substitute.line, { desc = "Substitute line" })
+          keymap.set("n", "<leader>nS", substitute.eol, { desc = "Substitute to end of line" })
+          keymap.set("x", "<leader>ns", substitute.visual, { desc = "Substitute in visual mode" })
      end,
 }
-
